2ee1ecd458a278c342db01ab4f5d0b3194eb017a
ss6666ss <marinephoto2012@gmail.com>
2022-04-21 22:21:24 +0900

    dsds

--- menu/forms.py
+++ menu/forms.py
@@ -5,4 +5,8 @@ class MenuForm(forms.ModelForm):
     class Meta:
         model = Menu
         fields   = ['name', 'calorie', 'protein', 'fat', 'carbohydrates', 'dietary_fiber']
+
+class FindForm(forms.Form):
+    find = forms.CharField(label='Find', required=False, \
+        widget=forms.TextInput(attrs={'class':'form-control'}))
         
--- menu/views.py
+++ menu/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import redirect
 from .forms import MenuForm
 from .models import Menu
+from .forms import FindForm
 
 def index(request):
     data = Menu.objects.all()
@@ -38,4 +39,33 @@ def edit(request, num):
         'form': MenuForm(instance=obj),
     }
     return render(request, 'menu/edit.html', params)
-    
+
+def delete(request, num):
+    menu = Menu.objects.get(id=num)
+    if (request.method == 'POST'):
+        menu.delete()
+        return redirect(to='/menu')
+    params = {
+        'title': 'Menu',
+        'id': num,
+        'obj': menu,
+    }
+    return render(request, 'menu/delete.html', params)
+
+def find(request):
+    if (request.method == 'POST'):
+        form = FindForm(request.POST)
+        find = request.POST['find']
+        data = Friend.objects.filter(name=find)
+        msg = 'Result: ' + str(data.count())
+    else:
+        msg = 'search words...'
+        form = FindForm()
+        data = Menu.objects.all()
+    params = {
+        'title': 'Menu',
+        'message': msg,
+        'form': form,
+        'data': data,
+    }
+    return render(request, 'hello/find.html', params)

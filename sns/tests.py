10afe57c2d8de712688f54bbe08d8af34477c473
ss6666ss <marinephoto2012@gmail.com>
2022-06-25 15:01:51 +0900

    sfdasa

--- sns/tests.py
+++ sns/tests.py
@@ -1,13 +1,36 @@
 from django.test import TestCase
 
+from django.contrib.auth.models import User
+from .models import Message
+
 class SnsTests(TestCase):
 
+    @classmethod 
+    def setUpClass(cls):
+        super().setUpClass()
+        (usr, grp) = cls.create_user_and_group()
+        cls.create_messsage(usr, grp)
+
+    @classmethod
+    def create_user_and_group(cls):
+        User(username = "public", password = "p6u6b6l6i6c", is_staff = False, is_active = True).save()
+        pb_usr = User.objects.filter(username = 'public').first()
+        Group(title = 'public', owner_id = pb_usr.id).save
+        pb_grp = Group.objects.filter(title = 'public').first()
+        User(username = "test",password = "test", is_staff = True, is_active = True).save()
+        usr = User.objects.filter(username = 'test').first()
+
+        return (usr, pb_grp)
+
+    def create_message(cls, usr, grp):
+        Message(content = 'this is test message.', owner_id = usr.id, group_id = grp.id).save()
+        Message(content = 'test', owner_id = usr.id, group_id = grp.id).save()
+        Message(content = "ok", owner_id = usr.id, group_id = grp.id).save()
+        Message(contnt = "ng", owner__id = usr.id, group_id = grp.id).save()
+        Message(content = 'finish', owner_id = usr.id, group_id = grp.id).save()
+
     def test_check(self):
-        x = True
-        self.assertTrue(x)
-        y = 100
-        self.assertGreater(y, 0)
-        arr = [10, 20, 30]
-        self.assertIn(20, arr)
-        nn = None
-        self.assertIsNone(nn)
+        usr = User.objects.first()
+        self.aasertIsNotNone(usr)
+        msg = Message.objects.first()
+        self.assertIsNotNone(msg)
